8450a5b39de020766f2b60d820d9cee4f9fcc617
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-10-12 21:34:08 -0400

    "Update DashboardWrapper component to include sidebar and styling" + NavBar intialization

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import NavBar from "@/app/(components)/NavBar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
-  return <div>DashboardWrapper {children}</div>;
+  return (
+    <div className={` light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      Sidebar
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        <NavBar />
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default DashboardWrapper;
