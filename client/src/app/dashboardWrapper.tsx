c8023cb4296f69ca4e1e6e08067b2cb4bafa938c
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-10-15 23:51:56 -0400

    "Refactor NavBar component and update DashboardLayout"

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect } from "react";
-import NavBar from "@/app/(components)/NavBar";
+import Navbar from "@/app/(components)/NavBar";
 import Sidebar from "@/app/(components)/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
 
@@ -10,11 +10,12 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     (state) => state.global.isSidebarCollapsed
   );
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("light");
     }
   });
 
@@ -22,15 +23,15 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     <div
       className={`${
         isDarkMode ? "dark" : "light"
-      } light flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
     >
       <Sidebar />
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
-          isSidebarCollapsed ? "md:pl-24`" : "md:pl-72"
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
         }`}
       >
-        <NavBar />
+        <Navbar />
         {children}
       </main>
     </div>
