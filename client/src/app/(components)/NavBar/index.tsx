108bd4eb4ce603e9b5424fdbb00b7d9a59dd2c3d
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-10-18 00:56:54 -0400

    "Update username in Navbar from 'Ed Roh' to 'No Ls'"

--- client/src/app/(components)/NavBar/index.tsx
+++ client/src/app/(components)/NavBar/index.tsx
@@ -67,7 +67,7 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            <span className="font-semibold">Ed Roh</span>
+            <span className="font-semibold">No Ls</span>
           </div>
         </div>
         <Link href="/settings">
