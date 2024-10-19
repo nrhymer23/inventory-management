75d62466a3c1aca946a80f4a0ef8be38b4f76ccd
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-10-18 23:45:24 -0400

    "Added dashboardRoutes import and Setup API

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -9,6 +9,7 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 /* Route imports*/
 /* Configurations*/
 dotenv_1.default.config();
@@ -21,9 +22,7 @@ app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 /* Route imports*/
-app.get("/hello", (req, res) => {
-    res.send("hello world2");
-});
+app.use("/dashboard", dashboardRoutes_1.default); //http://localhost:8000/dashboard
 /* servers*/
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import dashboardRoutes from "./routes/dashboardRoutes";
 /* Route imports*/
 
 /* Configurations*/
@@ -18,9 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* Route imports*/
-app.get("/hello", (req, res) => {
-  res.send("hello world2");
-});
+app.use("/dashboard", dashboardRoutes); //http://localhost:8000/dashboard
 
 /* servers*/
 const port = process.env.PORT || 3001;
