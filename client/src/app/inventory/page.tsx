58da50a48a6345275fbd21d7cd4642b3d964dc48
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-11-07 23:26:38 -0500

    Continued Setting up Inventory page

---
+++ client/src/app/inventory/page.tsx
@@ -0,0 +1,34 @@
+import { useGetProductsQuery } from "@/state/api";
+import React from "react";
+import Header from "@/app/(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "productId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Product Name", width: 200 },
+];
+
+const Inventory = () => {
+  const { data: products, isError, isLoading } = useGetProductsQuery();
+
+  if (isLoading) {
+    return <div className="py-4">Loading...</div>;
+  }
+
+  if (isError || !products) {
+    return (
+      <div className="text-center text-red-500 py-4">
+        Failed to fetch products
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <Header name="Inventory" />
+      <DataGrid rows={products} columns={columns} />
+    </div>
+  );
+};
+
+export default Inventory;
