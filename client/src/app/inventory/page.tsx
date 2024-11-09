253fec5dc1992e105c8ec9a004b9a521ef3399f7
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-11-08 20:56:55 -0500

    "Added 'use client' directive and modified DataGrid component in Inventory page"

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useGetProductsQuery } from "@/state/api";
 import React from "react";
 import Header from "@/app/(components)/Header";
@@ -6,6 +8,26 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 const columns: GridColDef[] = [
   { field: "productId", headerName: "ID", width: 90 },
   { field: "name", headerName: "Product Name", width: 200 },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => `$${row.price}`,
+  },
+  {
+    field: "rating",
+    headerName: "Rating",
+    width: 110,
+    type: "number",
+    valueGetter: (value, row) => (row.rating ? row.rating : "N/A"),
+  },
+  {
+    field: "stockQuanity",
+    headerName: "Stock Quantity",
+    width: 150,
+    type: "number",
+  },
 ];
 
 const Inventory = () => {
@@ -26,7 +48,12 @@ const Inventory = () => {
   return (
     <div className="flex flex-col">
       <Header name="Inventory" />
-      <DataGrid rows={products} columns={columns} />
+      <DataGrid
+        rows={products}
+        columns={columns}
+        getRowId={(row) => row.productId}
+        checkboxSelection
+      />
     </div>
   );
 };
