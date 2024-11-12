745a2c05fd812d0c0c5a902fe0fb477380c3036f
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-11-11 23:01:20 -0500

    "Added Header and Rating components, PlusCircleIcon, and Create Product button to Products page"

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useGetProductsQuery } from "@/state/api";
-import { SearchIcon } from "lucide-react";
+import { PlusCircleIcon, SearchIcon } from "lucide-react";
 import { useState } from "react";
+import Header from "@/app/(components)/Header";
+import Rating from "../(components)/Rating";
 
 const Products = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [isModalOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const {
     data: products,
@@ -25,21 +27,62 @@ const Products = () => {
     );
   }
 
-  return;
-  <div className="mx-auto pb-5 w-full">
-    {/*Search Bar*/}
-    <div className="mb-6">
-      <div className="flex items-center border-gray-200 rounded">
-        <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
-        <input
-          className="w-full py-2 px-4 rounded bg-white"
-          placeholder="Search products..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+  return (
+    <div className="mx-auto pb-5 w-full">
+      {/*Search Bar*/}
+      <div className="mb-6">
+        <div className="flex items-center border-gray-200 rounded">
+          <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+          <input
+            className="w-full py-2 px-4 rounded bg-white"
+            placeholder="Search products..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      {/*Header*/}
+      <div className="flex justify-between items-center mb-6">
+        <Header name="Products" />
+        <button
+          className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+          onClick={() => setIsModalOpen(true)}
+        >
+          <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" /> Create
+          Product
+        </button>
+      </div>
+      {/*Body Products List*/}
+      <div className="grid grid-cols-1 sm:grids-cols-2 lg-grid-cols-3 gap-10 justify-between">
+        {isLoading ? (
+          <div>Loading... </div>
+        ) : (
+          products?.map((product) => (
+            <div
+              key={product.productId}
+              className="border shadow rounded-md p-4 max-w-full w-full mx-auto"
+            >
+              <div className="flex flex-col items-center">
+                img
+                <h3 className="text-lg text-gray-900 font-semibold">
+                  {product.name}
+                </h3>
+                <p className="text-gray-800">${product.price.toFixed(2)}</p>
+                <div className="text-sm text-gray-600 mt-1">
+                  Stock: {product.stockQuantity}
+                </div>
+                {product.rating && (
+                  <div className="flex items-center mt-2">
+                    <Rating rating={product.rating} />
+                  </div>
+                )}
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Products;
