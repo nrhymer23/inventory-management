a4bd09590522fee21b7eea43ba81e522eb1f66e7
nrhymer23 <38295535+nrhymer23@users.noreply.github.com>
2024-11-18 23:35:08 -0500

    Started Settings page

---
+++ client/src/app/settings/page.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+type UserSetting = {
+  label: string;
+  value: string | boolean;
+  type: "text" | "toggle";
+};
+
+const mockSettings: UserSetting[] = [
+  { label: "Username", value: "john_doe", type: "text" },
+  { label: "Email", value: "john.doe@example.com", type: "text" },
+  { label: "Notification", value: true, type: "toggle" },
+  { label: "Dark Mode", value: false, type: "toggle" },
+  { label: "Language", value: "English", type: "text" },
+];
+
+const Settings = () => {
+  const [userSettings, setUserSettings] = useState<UserSetting[]>(mockSettings);
+
+  const handleToggleChange = (index: number) => {
+    const settingsCopy = [...userSettings];
+    settingsCopy[index].value = !settingsCopy[index].value as boolean;
+    setUserSettings(settingsCopy);
+  };
+
+  return <div>page</div>;
+};
+
+export default Settings;
